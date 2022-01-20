4037b8401ed327ca3e9391ff13ce11ffc56d8a56
Saifuddin <sepehr@microcis.net>
2022-01-20 12:51:58 +0100

    Initial Commit

---
+++ MovieTime/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieTime
+//
+//  Created by SDN SPR on 1/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieTime/MovieTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTimeApp.swift
+//  MovieTime
+//
+//  Created by SDN SPR on 1/20/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
